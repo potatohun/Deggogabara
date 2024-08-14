5909ac9c0963660f6405b08f398bdc042de34862
potato_hun <jihunranju@gmail.com>
2024-08-14 11:02:55 +0900

    0814
    BIC F5 리셋 버튼 추가

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -49,5 +50,13 @@ public class GameManager : MonoBehaviour
     {
         ChapterManager ChapterManager  = GameObject.Find("ChapterManager").GetComponent<ChapterManager>();
         map = ChapterManager.stageList[ChapterManager.selectedStage].GetComponent<Stage>().map;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F5))
+        {
+            SceneManager.LoadScene("Title");
+        }
     }
 }
